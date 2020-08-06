dc34a97850dad3c13f3b903357cda3f273c8da2d
Elvin Nunez <eanunez25@gmail.com>
2020-08-05 22:24:05 -0500

    create sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create(username: params[:user][:username])
+    redirect_to @user
   end
 
   def show
+    @users = User.all
   end
 end
