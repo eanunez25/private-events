647dba9e1da7ee98e86465f12945a37f4b31ba43
Elvin Nunez <eanunez25@gmail.com>
2020-08-08 10:42:41 -0500

    create sign in function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(username: params[:username])
+    if user
+      session[:user_id] = user.id 
+      redirect_to user_path(user.id), notice: "Logged in"
+    else
+      flash.now.alert = "Email does not exist"
+      render "new"
+    end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :sessions, only: [:create]
+  resources :sessions
   resources :users
+
+  get "signup", to: "users#new",        as: "signup"
+  get "login",  to: "sessions#new",     as: "login"
+  get "logout", to: "sessions#destroy", as: "logout"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
